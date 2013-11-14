b852b7bc00e4f8a20cd67c5cdc04f291a37a5f2b
Valeria Lepina <divergence082@gmail.com>
2013-11-14 15:04:29 +0400

    modified for npm

---
+++ etc/index.d
@@ -0,0 +1,10 @@
+
+util/util.js
+
+util/object/safe-object.js
+util/object/array-cache.js
+util/object/object-cache.js
+util/object/string-cache.js
+
+util/uid/i-id-generator.js
+util/uid/prefix-id-generator.js
