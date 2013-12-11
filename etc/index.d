4c248f6d7aa0eb8f3af37b0cc5a146ac3d83a72f
Alexey Korkota <aleksey.k@livetex.com>
2013-12-11 17:40:52 +0400

    /0

--- etc/index.d
+++
@@ -1,6 +0,0 @@
-
-cli/cli.js
-cli/logger.js
-cli/config.js
-cli/args/args.js
-
--- test/args.js
+++
@@ -1,7 +0,0 @@
-var cli = require('../bin');
-
-cli.args.extractValues(process.argv);
-
-console.log(cli.args.getString('a'));
-console.log(cli.args.getString('b'));
-console.log(cli.args.getString('c'));
